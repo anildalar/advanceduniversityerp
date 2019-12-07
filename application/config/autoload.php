5711cbaa4a852201901675cbc649a243ae686d62
Prthvi <prithvi.bansal1000@gmail.com>
2019-12-07 09:40:33 +0530

    GEt TEachers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('MY_Model');
+$autoload['model'] = array('MY_Model','Teacher_model');
--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,19 +1,23 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Teacher extends Base_Controller {
+class Api extends Base_Controller {
 	//1. Property (Variable)
 	
 	//2. Constructor
 	public function __construct(){
 		parent::__construct();
-		//$this->load->library('customlib');
-		//$this->load->model('News_model');
+		
 	}
 	
 	//3. Method (Function) Area
 	public function getTeachers()
 	{
+		$token = $this->input->get('token');
+		$uid = $this->input->get('university_id');
 		
+		$data = $this->Teacher_model->getTeachers($uid);
+		
+		echo json_encode($data);
 	}
 }
