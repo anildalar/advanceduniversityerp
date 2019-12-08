9b65bd9f53f51df8f93c2cd68e955253e6c2b73d
anil <dollorinfotech@gmail.com>
2019-12-08 17:00:29 +0530

    Another Commit Database

--- application/views/university/theme6/home.php
+++ application/views/university/theme6/home.php
@@ -678,42 +678,5 @@
 		<script src="<?php echo base_url('assets/frontend/university/theme6/'); ?>js/core.min.js"></script>
 		<script src="<?php echo base_url('assets/frontend/university/theme6/'); ?>js/script.js"></script>
 
-		<!--LIVEDEMO_00 -->
-
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-7078796-5']);
-			_gaq.push(['_trackPageview']);
-			(function() {
-				var ga = document.createElement('script');
-				ga.type = 'text/javascript';
-				ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'https://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(ga, s);
-			})();
-		</script>
-
 	</body>
-	<!-- Google Tag Manager -->
-	<noscript>
-		<iframe src="<?php echo base_url('assets/frontend/university/theme6/'); ?>//www.googletagmanager.com/ns.html?id=GTM-P9FT69" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-	</noscript>
-	<script>
-		(function(w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({
-				'gtm.start': new Date().getTime(),
-				event: 'gtm.js'
-			});
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src = '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-P9FT69');
-	</script>
-	<!-- End Google Tag Manager -->
-
 </html>
